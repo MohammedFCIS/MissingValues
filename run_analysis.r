b1439e437044d6480b763d320894e8cdca941888
Mohammed Ali <moh_fcis@yahoo.com>
2016-04-16 09:53:17 +0200

    set Age

--- run_analysis.r
+++ run_analysis.r
@@ -13,3 +13,8 @@ titanic = tbl_df(read.csv("data/titanic_original.csv", stringsAsFactors = FALSE)
 #1: Port of embarkationtit
 ####################################
 titanic$embarked = ifelse(titanic$embarked == '', 'S', titanic$embarked)
+
+####################################
+#2: Age
+####################################
+titanic$age = ifelse(is.na(titanic$age), mean(titanic$age, na.rm = TRUE), titanic$age) 
